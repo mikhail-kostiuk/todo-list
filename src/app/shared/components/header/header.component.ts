19360557090d0c6776a71fbbc736eed3447fbc9e
Mykhailo Kostiuk <mykhailo_kostiuk@epam.com>
2020-05-04 00:24:17 +0300

    Refactor header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import User from '../../interfaces/User';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(public authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout(): void {
+    this.authService.signOut();
   }
-
 }
